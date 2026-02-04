fd9e65132da8bad968cb75e66ccab4435548da45
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2026-02-04 10:14:33 +0200

    Solve romanToInt -> easy

--- js/easy.js
+++ js/easy.js
@@ -16,3 +16,31 @@
 //   return x === result;
 // };
 // console.log(isPalindrome(121));
+
+//& 3-  Roman to Integer
+var romanToInt = function (s) {
+  const romanNumerals = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const current = romanNumerals[s[i]];
+    const next = romanNumerals[s[i + 1]];
+
+    if (next > current) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
+};
