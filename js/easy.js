688a5b2c232549805a165c1b9d7b120afe197fd5
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2026-02-05 17:34:46 +0200

    Solve Running Sum of 1d Array -> easy

--- js/easy.js
+++ js/easy.js
@@ -18,49 +18,58 @@
 // console.log(isPalindrome(121));
 
 //& 3-  Roman to Integer
-var romanToInt = function (s) {
-  const romanNumerals = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
+// var romanToInt = function (s) {
+//   const romanNumerals = {
+//     I: 1,
+//     V: 5,
+//     X: 10,
+//     L: 50,
+//     C: 100,
+//     D: 500,
+//     M: 1000,
+//   };
 
-  let total = 0;
+//   let total = 0;
 
-  for (let i = 0; i < s.length; i++) {
-    const current = romanNumerals[s[i]];
-    const next = romanNumerals[s[i + 1]];
+//   for (let i = 0; i < s.length; i++) {
+//     const current = romanNumerals[s[i]];
+//     const next = romanNumerals[s[i + 1]];
 
-    if (next > current) {
-      total -= current;
-    } else {
-      total += current;
-    }
-  }
+//     if (next > current) {
+//       total -= current;
+//     } else {
+//       total += current;
+//     }
+//   }
 
-  return total;
-};
+//   return total;
+// };
 //& 4-  Longest Common Prefix
-var longestCommonPrefix = function (strs) {
-  if (strs.length === 0) return "";
+// var longestCommonPrefix = function (strs) {
+//   if (strs.length === 0) return "";
 
-  let prefix = "";
-  // ["flower", "flow", "flight"]
-  for (let i = 0; i < strs[0].length; i++) {
-    let char = strs[0][i];
+//   let prefix = "";
+//   // ["flower", "flow", "flight"]
+//   for (let i = 0; i < strs[0].length; i++) {
+//     let char = strs[0][i];
 
-    for (let j = 1; j < strs.length; j++) {
-      if (strs[j][i] !== char) {
-        return prefix;
-      }
-    }
+//     for (let j = 1; j < strs.length; j++) {
+//       if (strs[j][i] !== char) {
+//         return prefix;
+//       }
+//     }
 
-    prefix += char;
-  }
+//     prefix += char;
+//   }
 
-  return prefix;
-};
+//   return prefix;
+// };
+//& 5-  Running Sum of 1d Array
+// var runningSum = function (nums) {
+//   let runningSum = [];
+//   runningSum.push(nums[0]);
+//   for (let i = 1; i < nums.length; i++) {
+//     runningSum.push(nums[i] + runningSum[i - 1]);
+//   }
+//   return runningSum;
+// };
