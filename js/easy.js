54eb910490ce523930241111cb4a5381dfa5a37c
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2026-02-07 15:51:10 +0200

    Solve Counter -> easy

--- js/easy.js
+++ js/easy.js
@@ -73,3 +73,13 @@
 //   }
 //   return runningSum;
 // };
+//& 6- Counter
+// var createCounter = function (n) {
+//   return function () {
+//     return n++;
+//   };
+// };
+// const counter = createCounter(5);
+
+// console.log(counter());
+// console.log(counter());
