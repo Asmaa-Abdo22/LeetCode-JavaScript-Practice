043da5ecbc17a320fc8e4997e020e2e3f4fbc375
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2026-06-02 05:05:28 +0300

    Solve Longest Substring Without Repeating Characters -> Medium

--- js/medium.js
+++ js/medium.js
@@ -1,7 +1,25 @@
 //& 1- Reverse Integer
-var reverse = function (x) {
-  let r = Number(Math.abs(x).toString().split("").reverse().join(""));
+// var reverse = function (x) {
+//   let r = Number(Math.abs(x).toString().split("").reverse().join(""));
 
-  return r > 2147483647 ? 0 : r * Math.sign(x);
+//   return r > 2147483647 ? 0 : r * Math.sign(x);
+// };
+// console.log(reverse(-123));
+//& 2- Longest Substring Without Repeating Characters
+var lengthOfLongestSubstring = function (s) {
+  let maxLength = 0;
+  for (let i = 0; i < s.length; i++) {
+    let substr = "";
+
+    for (let j = i; j < s.length; j++) {
+      if (substr.includes(s[j])) {
+        break;
+      }
+
+      substr += s[j];
+      maxLength = Math.max(maxLength, substr.length);
+    }
+  }
+
+  return maxLength;
 };
-console.log(reverse(-123));
