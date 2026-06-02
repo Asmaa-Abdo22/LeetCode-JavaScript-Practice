b81d4bfd8b18ffd62cc10f3f7820e0093dbccaca
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2026-06-02 04:45:15 +0300

    Solve Move Zeroes -> easy

--- js/easy.js
+++ js/easy.js
@@ -326,18 +326,34 @@
 //   return false;
 // };
 //& 28- Max Consecutive Ones
-var findMaxConsecutiveOnes = function (nums) {
-  let count = 0;
-  let max = 0;
+// var findMaxConsecutiveOnes = function (nums) {
+//   let count = 0;
+//   let max = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 1) {
-      count++;
-      max = Math.max(max, count);
-    } else {
-      count = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === 1) {
+//       count++;
+//       max = Math.max(max, count);
+//     } else {
+//       count = 0;
+//     }
+//   }
+
+//   return max;
+// };
+//& 29- Move Zeroes
+var moveZeroes = function(nums) {
+    let insertPos = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[insertPos] = nums[i];
+            insertPos++;
+        }
     }
-  }
 
-  return max;
+    while (insertPos < nums.length) {
+        nums[insertPos] = 0;
+        insertPos++;
+    }
 };
