25e402c0b9596e6700f52d33acabb6f9b61f3a29
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2026-06-08 06:38:55 +0300

    feat: Letter Combinations of a Phone Number -> Medium

--- js/medium.js
+++ js/medium.js
@@ -24,16 +24,44 @@
 //   return maxLength;
 // };
 //& 3- Find All Duplicates in an Array
-var findDuplicates = function (nums) {
-  const count = {};
-  const result = [];
-  for (let num of nums) {
-    count[num] = (count[num] || 0) + 1;
-  }
-  for (let key in count){
-    if(count[key]===2){
-      result.push(Number(key))
+// var findDuplicates = function (nums) {
+//   const count = {};
+//   const result = [];
+//   for (let num of nums) {
+//     count[num] = (count[num] || 0) + 1;
+//   }
+//   for (let key in count){
+//     if(count[key]===2){
+//       result.push(Number(key))
+//     }
+//   }
+//   return result
+// };
+var letterCombinations = function (digits) {
+  if (!digits.length) return [];
+  let digitsArr = digits.split("").map((item) => Number(item));
+  const phoneMap = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+  let resultArr = [];
+  let increased = digitsArr.map((item) => phoneMap[item]); // ["abc", "def"]
+  resultArr = increased[0].split("");  // ["a","b","c"]
+  for (let i = 1; i < increased.length; i++) {
+    let temp = [];
+    for (let j = 0; j < resultArr.length; j++) {
+      for (let k = 0; k < increased[i].length; k++) {
+        temp.push(resultArr[j] + increased[i][k]);
+      }
     }
+    resultArr = temp;
   }
-  return result
+
+  return resultArr;
 };
